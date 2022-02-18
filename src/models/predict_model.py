0a3575dd82f4a9861f98cae681cda332843349f3
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-02-18 13:04:20 +0100

    updates testing

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -13,7 +13,6 @@ DATA_DIR = os.path.join(ROOT_DIR, "data", "processed")
 
 def test(model_path: str):
     tweets_df_test = torch.load(os.path.join(DATA_DIR, "test.pt"))
-    ids = pd.read_csv("/Users/max/Documents/Projects/dis_tweets/data/raw/test.csv")
     tweets_df_test["target"] = None
     tokenizer = transformers.DistilBertTokenizer.from_pretrained("distilbert-base-cased")
     test_dataset = TwitterDataset(tweets_df_test, tokenizer, max_len=100)
@@ -25,7 +24,7 @@ def test(model_path: str):
     pred = np.argmax(raw_pred, axis=1)
     out = pd.DataFrame()
     out["target"] = pred
-    out["id"] = ids["id"]
+    out["id"] = tweets_df_test["id"]
     save_dir = os.path.join(get_root_path(), "reports", "submissions", "test_pred.csv")
     out.to_csv(save_dir, index=False)
 
